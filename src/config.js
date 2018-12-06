98a4f92eeb4816572a0799d3aee380de0cd8a4c1
sgerami <sg@inspace.tv>
2018-12-06 18:49:27 +0000

    Add license.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2018 inSpace Technologies Ltd
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
 const config = {
   scaleFactor(level) {
     return 0.6 ** level;
